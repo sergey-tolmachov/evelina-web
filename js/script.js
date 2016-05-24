f29f4a9813f31c608d10066d170b961bbb001f3d
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-05-24 11:28:08 +0300

    finish close
    almost whole layout finished

--- js/script.js
+++ js/script.js
@@ -12,3 +12,33 @@ $(document).ready(function() {
 		$(".read-more").fadeOut();
 	});
 });
+
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top-150;
+        
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $("#mail").on("click","a", function (event) {
+        event.preventDefault();
+
+        var id  = $(this).attr('href'),
+
+            top = $(id).offset().top-150;
+        
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
